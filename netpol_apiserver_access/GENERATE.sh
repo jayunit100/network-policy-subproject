9bfb3cc0a9e2ce4026b1ef78fab0f8580dffa1d5
jay vyas <jayunit100.github@gmail.com>
2020-08-07 13:19:15 -0400

    first user story

---
+++ netpol_apiserver_access/GENERATE.sh
@@ -0,0 +1,5 @@
+# THis will be a shell script that
+- spins up a kind cluster
+- creates a pod that has a policy applied to it
+- demonstrates the problem of not being able to access the apiserver by default
+
